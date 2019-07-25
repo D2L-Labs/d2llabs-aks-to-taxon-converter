1896ffa66dc2da98e6f3a803a58ff563c8dcd8c6
Brian Cepuran <brian@d2l.com>
2019-07-25 07:17:31 -0400

    added ASN input file formatter

--- index-asn.js
+++ index-asn.js
@@ -1,22 +1,30 @@
 'use strict'
 
-var fs = require('fs');
-var parse = require('csv-parse');
-var iconv = require('iconv-lite');
+let fs = require('fs');
+let parse = require('csv-parse');
+let iconv = require('iconv-lite');
 
-var aErrors = new Array();
+let aErrors = new Array();
 
 //stop-gap solution to handling of invalid characters - now may be unnecessary due to improved file encoding handling
 //left for now to help ensure that script can execute without crashing
 function removeInvalidChars( inputString, inputSource, loggingString ) {
-	var outputString = inputString;
+	let outputString = inputString;
 	if( !loggingString ) {
 			loggingString = inputString;
 	}
 
+	if( outputString ) {
+		/*
+			special case character removal - do not log as error
+			\uFEFF = no-width, non-breaking space
+		*/
+		outputString = outputString.replace(/[\uFEFF]/g,"");
+	}
+
 	if( !(new RegExp(/^((\u0009|\u000A|\u000D|[\u0020-\uD7FF])|([\uD800-\uDBFF][\uDC00-\uDFFF]))*$/).test(outputString)) ) {
 		outputString = outputString.replace(/[^\u0009\u000A\u000D\u0020-\uD7FF\uD800-\uDBFF\uDC00-\uDFFF]/g, "");
-		aErrors.push( inputSource + ":" + loggingString + '\r\n' + "Fix:" + outputString)
+		aErrors.push( "O: " + loggingString + '\r\n' + "F: " + outputString)
 	}
 	if( outputString ) {
 		outputString = outputString.trim();
@@ -24,17 +32,15 @@ function removeInvalidChars( inputString, inputSource, loggingString ) {
 	return outputString;
 }
 
-var arrRows = new Array(); //an array to contain the top level items as returned by the processLearningItem function
-
 //pull input and output filenames from the command line - expected format is
 //USAGE:
-//		node index.js INPUTFILENAME [OUTPUTFILENAME]
+//		node index.js INPUTFILENAME [MAPPINGFILENAME]
 //
 //output file will default to the input file name with a .xml file extension added to it
-var inputFileName = "", outputFileNameTemplate = "", outputPlaceHolder = "~!@!~", errorFileName = "", interimFileName, outputFileName, outputFolderName;
+let inputFileName = "", outputFileNameTemplate = "", outputPlaceHolder = "~!@!~", errorFileName = "", interimFileName, outputFileName, outputFolderName;
 
 if( process.argv.length < 3 ) {
-	console.error( "ERROR: Missing command line arguments.\r\n\r\nUSAGE: \r\n node index.js INPUTFILENAME [OUTPUTOLDERNAME]\r\n\r\OUTPUTOLDERNAME is optional, if ommitted the utility will use INPUTFILENAME and append a .xml extension\r\n");
+	console.error( "ERROR: Missing command line arguments.\r\n\r\nUSAGE: \r\n node index.js INPUTFILENAME [MAPPINGFILENAME]\r\n\r\nMAPPINGFILENAME is optional\r\n");
 	return;
 }
 
@@ -42,7 +48,7 @@ inputFileName = process.argv[2];
 outputFolderName = inputFileName + "-output";
 outputFileNameTemplate = outputFolderName + "/" + outputPlaceHolder + ".txt";
 
-let mappingsFile = "", mapMappings = new Map();
+let mappingsFile = "", mapMappings = new Map(), mapExclusions = new Map();
 if( process.argv.length >= 4 ) {
 	mappingsFile = process.argv[3];
 
@@ -50,6 +56,9 @@ if( process.argv.length >= 4 ) {
 	for( let mapping of objMappings ) {
 		for( let prefix of mapping.prefixes ) {
 			mapMappings.set(prefix, mapping.group);
+		}
+		for( let exclusion of mapping.exclusions ) {
+			mapExclusions.set(exclusion, mapping.group);
 		}
 	}
 }
@@ -87,10 +96,12 @@ let arraySorter = (e1, e2) => {
 }
 
 let mapOverall = new Map();
+let mapOverallExclusions = new Map();
+let targetMap;
 
 //parse the input CSV file
 //NOTE: File is expected to have a header row that should be ignored
-var parser = parse( {delimiter: ','}, (err, data) => {
+let parser = parse( {delimiter: ','}, (err, data) => {
 
 	if( err ) {
 		console.error(err);
@@ -117,26 +128,26 @@ var parser = parse( {delimiter: ','}, (err, data) => {
 		row.unshift(grouping);
 	}
 
-	var numRows = data.length;
+	let numRows = data.length;
 	if( numRows <= 1 ) return;	//no work to do here
 
-	var header = data[1];
-	var depth = header.length;
+	let header = data[1];
+	let depth = header.length;
 	if( depth <=1 ) return;	//no work to do here
 
-	var maxDepth = 0, maxDepthIndex = 0, currentDepth = 0;
+	let maxDepth = 0, maxDepthIndex = 0, currentDepth = 0;
 	const HEADER_ROW = "Hierarchy|EducationLevel|Notation|Description";
-	//arrRows.push(HEADER_ROW);
+
 //4GM: General Music - 4,4GM.A: Skills and Techniques/Performance ,"4GM.A.1: sing, alone and with others, a varied repertoire of music",4GM.A.1.a: sing melodies expressively using appropriate head voice (accompanied and unaccompanied) demonstrating awareness of the tonal center
 	//Hierarchy|EducationLevel|Notation|Description
 	//|4||General Music
 	//\t|4|4GM.A|Skills and Techniques/Performance
 	//\t\t|4|4GM.A.1|sing, alone and with others, a varied repertoire of music
 	//\t\t|4|4GM.A.1.a|sing melodies expressively using appropriate head voice (accompanied and unaccompanied) demonstrating awareness of the tonal center
-	var currentFirstLevel = "", currentSecondLevel = "", currentThirdLevel = "", currentFourthLevel, currentGradeLevel = "", currentNotation = "", placeholderVal, prevPlaceholderVal = "";
-	var prevFirstLevel = "", prevSecondLevel = "", prevThirdLevel = "";
+	let currentFirstLevel = "", currentSecondLevel = "", currentThirdLevel = "", currentFourthLevel, currentGradeLevel = "", currentNotation = "", placeholderVal, prevPlaceholderVal = "";
+	let prevFirstLevel = "", prevSecondLevel = "", prevThirdLevel = "";
 
-	var eduLevel, notation, desc;
+	let eduLevel, notation, desc;
 	
 	//go through each row in the array of items
 	for( var i = 0; i < numRows; i++ ) {
@@ -157,26 +168,26 @@ var parser = parse( {delimiter: ','}, (err, data) => {
 			eduLevel = HS_EDULEVEL;
 		}
 
-		// if( placeholderVal != prevPlaceholderVal  ) {
-		// 	if( prevPlaceholderVal != "" ) {
-		// 		outputFileName = outputFileNameTemplate.replace(outputPlaceHolder, prevPlaceholderVal.replace(/[\\/:*?\"<>|]/g,""));
-		// 		fs.writeFileSync(outputFileName, arrRows.join("\r\n"), options);
-
-		// 		arrRows = [];
-		// 		arrRows.push(HEADER_ROW);	
-		// 	}
-
-		// 	prevPlaceholderVal = placeholderVal;
-		// }
 		if( !mapOverall.has(placeholderVal) ) {
 			mapOverall.set(placeholderVal, new Array());
 			mapOverall.get(placeholderVal).push(HEADER_ROW);
+
+			mapOverallExclusions.set(placeholderVal, new Array());
 		}
+
 		if( currentFirstLevel != prevFirstLevel ) {
 			notation = currentFirstLevel.split(":")[0];
 			desc = removeInvalidChars(currentFirstLevel.split(":")[1], 1);
 
-			mapOverall.get(placeholderVal).push(`|${eduLevel}|${notation}|${desc}`);
+			targetMap = mapOverall;
+			for( let exclusion of mapExclusions.keys() ) {
+				if( currentFirstLevel.substr(0, exclusion.length) == exclusion ) {
+					targetMap = mapOverallExclusions;
+					break;
+				}
+			}
+
+			targetMap.get(placeholderVal).push(`|${eduLevel}|${notation}|${desc}`);
 
 			prevFirstLevel = currentFirstLevel;
 		}
@@ -185,7 +196,7 @@ var parser = parse( {delimiter: ','}, (err, data) => {
 			notation = currentSecondLevel.split(":")[0];
 			desc = removeInvalidChars(currentSecondLevel.split(":")[1], 2);
 
-			mapOverall.get(placeholderVal).push(`\t|${eduLevel}|${notation}|${desc}`);
+			targetMap.get(placeholderVal).push(`\t|${eduLevel}|${notation}|${desc}`);
 
 			prevSecondLevel = currentSecondLevel;
 		}
@@ -194,7 +205,7 @@ var parser = parse( {delimiter: ','}, (err, data) => {
 			notation = currentThirdLevel.split(":")[0];
 			desc = removeInvalidChars(currentThirdLevel.split(":")[1],3);
 
-			mapOverall.get(placeholderVal).push(`\t\t|${eduLevel}|${notation}|${desc}`);
+			targetMap.get(placeholderVal).push(`\t\t|${eduLevel}|${notation}|${desc}`);
 
 			prevThirdLevel = currentThirdLevel;
 		}
@@ -203,21 +214,24 @@ var parser = parse( {delimiter: ','}, (err, data) => {
 		desc = removeInvalidChars(currentFourthLevel.split(":")[1],4);
 
 		if( notation && desc ) {
-			mapOverall.get(placeholderVal).push(`\t\t|${eduLevel}|${notation}|${desc}`);
+			targetMap.get(placeholderVal).push(`\t\t|${eduLevel}|${notation}|${desc}`);
 		}
 
 	}
 
-	// if( currentFirstLevel != "" ) {
-	// 	outputFileName = outputFileNameTemplate.replace(outputPlaceHolder, placeholderVal.replace(/[\\/:*?\"<>|]/g,""));
-	// 	fs.writeFileSync(outputFileName, arrRows.join("\r\n"), options);
-
-	// 	arrRows = [];
-	// }
 
 	for( let placeholder of mapOverall.keys() ) {
-		outputFileName = outputFileNameTemplate.replace(outputPlaceHolder, placeholder.replace(/[\\/:*?\"<>|]/g,""));
-		fs.writeFileSync(outputFileName, mapOverall.get(placeholder).join("\r\n"), options);
+		if( mapOverall.get(placeholder).length > 1 ) {	//a length of 1 would be only the header
+			outputFileName = outputFileNameTemplate.replace(outputPlaceHolder, placeholder.replace(/[\\/:*?\"<>|]/g,""));
+			fs.writeFileSync(outputFileName, mapOverall.get(placeholder).join("\r\n"), options);
+		}
+	}
+
+	for( let placeholder of mapOverallExclusions.keys() ) {
+		if( mapOverallExclusions.get(placeholder).length > 0 ) {	//there are no header rows in the exclusions array
+			outputFileName = outputFileNameTemplate.replace(outputPlaceHolder, `_zz_excluded-${placeholder.replace(/[\\/:*?\"<>|]/g,"")}`);
+			fs.writeFileSync(outputFileName, mapOverallExclusions.get(placeholder).join("\r\n"), options);
+		}
 	}
 
 	var currentDate = new Date();
@@ -231,12 +245,6 @@ var parser = parse( {delimiter: ','}, (err, data) => {
 	}
 });
 
-//ensuring that the file encoding is one that will not cause issues
-// iconv.skipDecodeWarning = true;
-// var inputFileContents = fs.readFileSync(inputFileName, {encoding: "binary"});
-// var outputFileContents = iconv.decode(inputFileContents, "win1251");
-// fs.writeFileSync(interimFileName, outputFileContents);
-// fs.createReadStream( interimFileName ).pipe( parser );
 
 fs.createReadStream(inputFileName).pipe( parser );
 
